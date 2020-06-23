ca808cee180590a9edb270d3d42b83d261001a74
Jasson Alexander <jasson.alexander28@gmail.com>
2020-06-22 19:32:14 -0500

    data

---
+++ SlnExamenBiblioteca/BEUBibliotecaa/Transactions/VideoBLL.cs
@@ -0,0 +1,91 @@
+﻿using BEUBibliotecaa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEUBiblioteca.Transactions
+{
+    public class VideoBLL
+    {
+        public static Video Get(int? id)
+        {
+            Entities db = new Entities();
+            return db.Videos.Find(id);
+        }
+
+
+        public static void Create(Video v)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Videos.Add(v);
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static void Update(Video v)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Videos.Attach(v);
+                        db.Entry(v).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static void Delete(int? id)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        Video v = db.Videos.Find(id);
+                        db.Entry(v).State = System.Data.Entity.EntityState.Deleted;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+
+            }
+        }
+
+        public static List<Video> List()
+        {
+            Entities db = new Entities();
+            return db.Videos.ToList();
+        }
+    }
+}
